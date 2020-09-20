ce1e6c5bdf3755347a45f59f978b8cc3300a0056
hramtsov13 <hramtsov13@mail.ru>
2020-09-20 19:46:44 +0500

    components/active menu/sidebar hide

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <component v-bind:is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
 
+<script>
+import emptyLayout from '@/layouts/emptyLayout.vue';
+import mainLayout from '@/layouts/mainLayout.vue';
+
+export default {
+  computed: {
+    layout() {
+      return `${this.$route.meta.layout || 'empty'}-layout`;
+    },
+  },
+  components: {
+    emptyLayout,
+    mainLayout,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
 </style>
